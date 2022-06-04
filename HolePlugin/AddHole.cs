c0ae3cbf2fcc7c85865f53c4f9c9d89c94f64e94
Татьяна Буянова <tanyasilagina@gmail.com>
2022-06-05 02:32:22 +0300

    Добавьте файлы проекта.

---
+++ HolePlugin/AddHole.cs
@@ -0,0 +1,147 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.DB.Structure;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolePlugin
+{
+    [Transaction(TransactionMode.Manual)]
+    public class AddHole : IExternalCommand
+    {
+        
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document arDoc = commandData.Application.ActiveUIDocument.Document;
+            Document ovDoc = arDoc.Application.Documents.OfType<Document>().Where(x=>x.Title.Contains("ОВ")).FirstOrDefault();
+            if (ovDoc == null)
+            {
+                TaskDialog.Show("Ошибка","Не найден ОВ файл");
+                return Result.Cancelled;
+            }
+
+            FamilySymbol familySymbol = new FilteredElementCollector(arDoc)
+                .OfClass(typeof(FamilySymbol))
+                .OfCategory(BuiltInCategory.OST_GenericModel)
+                .OfType<FamilySymbol>()
+                .Where(x => x.FamilyName.Equals("Отверстие"))
+                .FirstOrDefault();
+            if(familySymbol == null)
+            {
+                TaskDialog.Show("Ошибка", "Не найдено семейство \"Отверстия\"");
+                return Result.Cancelled;
+
+            }
+
+            List<Duct> ducts = new FilteredElementCollector(ovDoc)
+                .OfClass(typeof(Duct))
+                .OfType<Duct>()
+                .ToList();
+            List<Pipe> pipes = new FilteredElementCollector(ovDoc)
+                .OfClass(typeof(Pipe))
+                .OfType<Pipe>()
+                .ToList();
+
+            View3D view3D = new FilteredElementCollector(arDoc)
+                .OfClass(typeof(View3D))
+                .OfType<View3D>()
+                .Where(x => !x.IsTemplate)
+                .FirstOrDefault();
+            if(view3D == null)
+            {
+                TaskDialog.Show("Ошибка", "Не найден 3D вид");
+                return Result.Cancelled;
+            }
+
+            ReferenceIntersector referenceIntersector = new ReferenceIntersector(new ElementClassFilter(typeof(Wall)), FindReferenceTarget.Element, view3D);
+            ReferenceIntersector referenceIntersector1 = new ReferenceIntersector(new ElementClassFilter(typeof(Wall)), FindReferenceTarget.Element, view3D);
+            Transaction ts = new Transaction(arDoc);
+            ts.Start("Расстановка отверстий");
+            foreach (Duct duct in ducts)
+            {
+                Line curve = (duct.Location as LocationCurve).Curve as Line;
+                XYZ point = curve.GetEndPoint(0);
+                XYZ direction = curve.Direction;
+                List<ReferenceWithContext> intersection = referenceIntersector.Find(point, direction)
+                     .Where(x => x.Proximity <= curve.Length)
+                     .Distinct(new ReferenceWithContextElementEqualityComparer())
+                     .ToList();
+                foreach (ReferenceWithContext refer in intersection)
+                {
+                    double proximity = refer.Proximity;
+                    Reference reference = refer.GetReference();
+                    Wall wall = arDoc.GetElement(reference.ElementId) as Wall;
+                    Level level = arDoc.GetElement(wall.LevelId) as Level;
+                    XYZ pointHole = point + (direction * proximity);
+
+
+                    FamilyInstance hole = arDoc.Create.NewFamilyInstance(pointHole, familySymbol, wall, level, StructuralType.NonStructural);
+                    Parameter width = hole.LookupParameter("Ширина");
+                    Parameter height = hole.LookupParameter("Высота");
+                    width.Set(duct.Diameter);
+                    height.Set(duct.Diameter);
+                }
+            }
+
+                foreach (Pipe pipe in pipes)
+                {
+                    Line line = (pipe.Location as LocationCurve).Curve as Line;
+                    XYZ point1 = line.GetEndPoint(0);
+                    XYZ direction1 = line.Direction;
+                    List<ReferenceWithContext> intersection1 = referenceIntersector.Find(point1, direction1)
+                         .Where(x => x.Proximity <= line.Length)
+                         .Distinct(new ReferenceWithContextElementEqualityComparer())
+                         .ToList();
+                    foreach (ReferenceWithContext refer in intersection1)
+                    {
+                        double proximity = refer.Proximity;
+                        Reference reference = refer.GetReference();
+                        Wall wall = arDoc.GetElement(reference.ElementId) as Wall;
+                        Level level = arDoc.GetElement(wall.LevelId) as Level;
+                        XYZ pointHole = point1 + (direction1 * proximity);
+
+
+                        FamilyInstance hole = arDoc.Create.NewFamilyInstance(pointHole, familySymbol, wall, level, StructuralType.NonStructural);
+                        Parameter width = hole.LookupParameter("Ширина");
+                        Parameter height = hole.LookupParameter("Высота");
+                        width.Set(pipe.Diameter);
+                        height.Set(pipe.Diameter);
+                    }
+                }
+            ts.Commit();
+            return Result.Succeeded;
+        }
+        public class ReferenceWithContextElementEqualityComparer : IEqualityComparer<ReferenceWithContext>
+        {
+            public bool Equals(ReferenceWithContext x, ReferenceWithContext y)
+            {
+                if (ReferenceEquals(x, y)) return true;
+                if (ReferenceEquals(null, x)) return false;
+                if (ReferenceEquals(null, y)) return false;
+
+                var xReference = x.GetReference();
+
+                var yReference = y.GetReference();
+
+                return xReference.LinkedElementId == yReference.LinkedElementId
+                           && xReference.ElementId == yReference.ElementId;
+            }
+
+            public int GetHashCode(ReferenceWithContext obj)
+            {
+                var reference = obj.GetReference();
+
+                unchecked
+                {
+                    return (reference.LinkedElementId.GetHashCode() * 397) ^ reference.ElementId.GetHashCode();
+                }
+            }
+        }
+    }
+}
